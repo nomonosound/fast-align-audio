dda3a76552d3a6a1f4c07c1c167e355257127820
iver56 <iver56@hotmail.com>
2023-06-30 13:12:33 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -7,4 +7,14 @@ setup(
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["fast_align_audio/_fast_align_audio_cffi.py:ffibuilder"],
     install_requires=["cffi>=1.0.0"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
